6388fac4ea3fccc5489aed964473af499091bad7
Matt Moyer <matthewrmoyer@gmail.com>
2016-01-06 11:01:43 -0700

    add interactions cow fade

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 $( document ).ready(function() {
 
+    $("div").fadeTo("fast" , .5);
+    $("a").fadeTo("fast", .5);
 
-    $("div").mouseenter(function() {
-        $("div").fadeTo('slow',1);
+
+	   
+    $("textarea").click(function(event) {
+        $("div").fadeTo(500,.2);
+        $("a").fadeTo(500,.2);
     });
-    $("div").mouseleave(function() {
-        $("div").fadeTo("slow", .5);
+    $("textarea").mouseleave(function() {
+        $("div").fadeTo(1000, .5);
+        $("a").fadeTo(1000, .5);
 });
 
     $( "a" ).click(function( event ) {
  
-        alert( "Thanks for visiting!" );
+        confirm( "Leave Moogle?" );
  
     });
     $("button").click(function(event){
-    	alert("MOOOOOO");
+    	    $("div").fadeTo("slow",0);
+            $("a").fadeTo("slow", 0);
+
 
     });
 
+    $(".cow").click(function(event){
+        $(".cow").fadeTo("slow",0);
 
+    });
+    
+
+    $("div").mouseover(function(){
+        $(this).fadeTo("slow", 1);
+
+    });
+
+    $("a").mouseover(function(){
+        $(this).fadeTo("fast", 1);
+
+    });
+
+    $("a").mouseleave(function(){
+        $(this).fadeTo("fast", .5);
+    });
+
+  
 
 
 
